402a6b578f21f0693033192aad72c1419cb2e59e
Oleksii Zelenko <52129260+AlexeyZelenko@users.noreply.github.com>
2020-05-13 14:08:07 +0300

    deploy

---
+++ precache-manifest.a812035ffce848bfe8e81d032e2f52cf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028bdcd5c2b0bc54eab2",
+    "url": "/vue-youtube-public/css/app.4e8e286f.css"
+  },
+  {
+    "revision": "b1806c511a1cd1a09184",
+    "url": "/vue-youtube-public/css/chunk-vendors.2955f2db.css"
+  },
+  {
+    "revision": "d264d318368a341ec89327182f68d4f6",
+    "url": "/vue-youtube-public/index.html"
+  },
+  {
+    "revision": "028bdcd5c2b0bc54eab2",
+    "url": "/vue-youtube-public/js/app.16473de6.js"
+  },
+  {
+    "revision": "2594c80577e2ea803335",
+    "url": "/vue-youtube-public/js/chunk-0aa3d7f6.bb365115.js"
+  },
+  {
+    "revision": "129e49edb88a01240d95",
+    "url": "/vue-youtube-public/js/chunk-10b86dd9.a67d9ff6.js"
+  },
+  {
+    "revision": "7016638052075dfd7cab",
+    "url": "/vue-youtube-public/js/chunk-2d217357.4781b2f4.js"
+  },
+  {
+    "revision": "4bc5d3558d2d6ec997f8",
+    "url": "/vue-youtube-public/js/chunk-2d217a99.ff0bd8c5.js"
+  },
+  {
+    "revision": "79b464b78c5873c092ad",
+    "url": "/vue-youtube-public/js/chunk-2d21a3d2.32b2f10a.js"
+  },
+  {
+    "revision": "dd6fa7857e8a148a00c5",
+    "url": "/vue-youtube-public/js/chunk-2d2259e5.5d6c5e71.js"
+  },
+  {
+    "revision": "3514ee59ee4409b5b2fe",
+    "url": "/vue-youtube-public/js/chunk-53c8ec65.85d470b7.js"
+  },
+  {
+    "revision": "50a74444d71e31d5a8ea",
+    "url": "/vue-youtube-public/js/chunk-5f4ad635.6c2bf37e.js"
+  },
+  {
+    "revision": "0b65e4bc9026e37fd92e",
+    "url": "/vue-youtube-public/js/chunk-c420df12.6a34b76f.js"
+  },
+  {
+    "revision": "b1806c511a1cd1a09184",
+    "url": "/vue-youtube-public/js/chunk-vendors.b7cbff4e.js"
+  },
+  {
+    "revision": "81ccd5cc53d522f5b163cbbeeb7ba7ab",
+    "url": "/vue-youtube-public/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-youtube-public/robots.txt"
+  }
+]);
