05552aa217ab72ae8f92846aec64124dcb996154
Oleksii Zelenko <52129260+AlexeyZelenko@users.noreply.github.com>
2020-05-13 13:49:25 +0300

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// файл vue.config.js расположен в корне вашего репозитория
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
+}
