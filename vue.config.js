9c8f348b33e96f28a96c5f326c01fc9a8b1deb45
Oleksii Zelenko <52129260+AlexeyZelenko@users.noreply.github.com>
2020-05-13 13:52:53 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
-// файл vue.config.js расположен в корне вашего репозитория
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/'
+    ? '/vue-youtube/'
+    : '/',
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
